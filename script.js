f080ccc045c5b4f30ba623fd691eb9520e4e2f77
JohanGTS <johangts02@gmail.com>
2022-09-15 16:56:31 -0400

    Diseño y lógica de agregarPalabra completado

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
-let conjuntoPalabras=["BUG","TI","HTML","JAVA","COOKIE","CURSOR","DATOS","ARCHIVO","CARPETA","HACKER",
+const palabraAgregada= document.getElementById("introducirPalabra");
+
+let conjuntoPalabras=["BUG","BINARIO","HTML","JAVA","COOKIE","CURSOR","DATOS","ARCHIVO","CARPETA","HACKER",
 "CSS","OBJETO","LINK","MENU","MODEM","RED"];
 
+let storedArray;
+
 let palabraSecreta;
+
+function home(){
+    window.location.href="index.html";
+}
+
+function iniciarJuego(){
+    window.location.href="areaJuego.html";
+}
+
+function irAgregarPalabra(){
+    window.location.href = 'agregarPalabra.html';
+}
+
 
 function generarAleatorio(){
     let longitud= conjuntoPalabras.length-1;
@@ -12,4 +29,31 @@ function generarAleatorio(){
 function obtenerPalabraSecreta()
 {
     return conjuntoPalabras[generarAleatorio()];
+}
+
+function prueba()
+{
+    storedArray=JSON.parse(sessionStorage.getItem("items"));
+    console.log(storedArray);
+}
+
+function agregarALista()
+{
+    let palabraVerificar=palabraAgregada.value.toUpperCase();
+    if(palabraVerificar.length>8||palabraVerificar.length<2)
+    {
+        alert("Palabra fuera de rango");
+        return;
+    }
+    storedArray=JSON.parse(sessionStorage.getItem("items"));
+    if(storedArray.includes(palabraVerificar))
+    {
+        alert("Palabra existente");
+        return;
+    }
+    
+    conjuntoPalabras.push(palabraVerificar);
+    window.sessionStorage.setItem("items", JSON.stringify(conjuntoPalabras));//guardamos el array en el cache
+
+    palabraAgregada.value="";
 }
